9d8d140c2a20d9aa73d4770a7139d6c5d2c9099b
Tomin <itoxa@MacBook-Pro.local>
2024-03-08 11:43:54 +0300

    run grpc server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,30 +2,21 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/itohin/gophkeeper/internal/server/adapters/db/postgres"
-	auth2 "github.com/itohin/gophkeeper/internal/server/usecases/auth"
+	"github.com/itohin/gophkeeper/internal/server/adapters/grpc"
+	"github.com/itohin/gophkeeper/internal/server/usecases/auth"
 	"github.com/itohin/gophkeeper/pkg/database"
 	"github.com/itohin/gophkeeper/pkg/hash/password"
 	"github.com/itohin/gophkeeper/pkg/jwt"
 	"github.com/itohin/gophkeeper/pkg/logger"
-	mailer2 "github.com/itohin/gophkeeper/pkg/mailer"
+	"github.com/itohin/gophkeeper/pkg/mailer"
 	"github.com/itohin/gophkeeper/pkg/otp"
 	"github.com/itohin/gophkeeper/pkg/uuid"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
-
-type PGXTransaction struct {
-}
-
-func NewPGXTransaction() *PGXTransaction {
-	return &PGXTransaction{}
-}
-
-func (t *PGXTransaction) Transaction(f func() error) error {
-	defer fmt.Println("defer call")
-	return f()
-}
 
 func main() {
 
@@ -40,22 +31,54 @@ func main() {
 	}
 	defer db.Pool.Close()
 
+	srv, err := setupServer(db, l)
+	if err != nil {
+		l.Fatal(err)
+	}
+	idleConnsClosed := make(chan struct{})
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	go func() {
+		<-sigint
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		srv.Stop(ctx)
+		close(idleConnsClosed)
+	}()
+
+	err = srv.Start()
+	if err != nil {
+		l.Fatal(err)
+	}
+	<-idleConnsClosed
+
+	db.Pool.Close()
+
+	l.Info("server shutdown gracefully")
+
+}
+
+func setupServer(db *database.PgxPoolDB, l logger.Logger) (*grpc.Server, error) {
+	authUseCase, err := setupAuth(db, l)
+	if err != nil {
+		return nil, err
+	}
+	return grpc.NewServer(authUseCase, l), nil
+}
+
+func setupAuth(db *database.PgxPoolDB, l logger.Logger) (*auth.AuthUseCase, error) {
 	usersRepo := postgres.NewUsersRepository(db)
 	sessionsRepo := postgres.NewSessionsRepository(db)
 	tx := database.NewPgxTransaction(db.Pool)
 	uuidGen := uuid.NewGoogleUUIDGenerator()
 	jwtGen, err := jwt.NewJWTGOManager("secret", 60*time.Second, 360*time.Second)
 	if err != nil {
-		l.Fatal(err)
+		return nil, err
 	}
 	passwordHash := password.NewBcryptPasswordHasher()
 	otpGen := otp.NewGOTPGenerator(9)
-	mailer := mailer2.NewSMTPMailer("from", "pass", "host", "port", l)
+	smtp := mailer.NewSMTPMailer("from@gmail.com", "", "localhost", "1025", l)
 
-	auth := auth2.NewAuthUseCase(passwordHash, uuidGen, otpGen, usersRepo, sessionsRepo, mailer, jwtGen, tx)
-	//err = auth.Register(context.Background(), "a@a.com", "password")
-	token, err := auth.Verify(context.Background(), "a@a.com", "PLYOGRTHJWCRGMQ", "unique_fingerprint")
-
-	fmt.Println("token: ", token, "error: ", err)
-
+	return auth.NewAuthUseCase(passwordHash, uuidGen, otpGen, usersRepo, sessionsRepo, smtp, jwtGen, tx), nil
 }
--- internal/server/adapters/grpc/grpc.go
+++ internal/server/adapters/grpc/grpc.go
@@ -1,24 +1,47 @@
 package grpc
 
 import (
+	"context"
 	"errors"
 	"github.com/itohin/gophkeeper/internal/server/usecases"
+	"github.com/itohin/gophkeeper/pkg/logger"
 	pb "github.com/itohin/gophkeeper/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"net"
 )
 
 type Server struct {
 	srv *grpc.Server
+	log logger.Logger
 }
 
-func NewServer(auth Auth) *Server {
+func NewServer(auth Auth, log logger.Logger) *Server {
 	srv := grpc.NewServer()
 	pb.RegisterAuthServer(srv, &AuthServer{
 		auth: auth,
 	})
 
-	return &Server{srv: srv}
+	return &Server{
+		srv: srv,
+		log: log,
+	}
+}
+
+func (s *Server) Start() error {
+	listen, err := net.Listen("tcp", ":3200")
+	if err != nil {
+		return err
+	}
+	s.log.Info("server started")
+	if err := s.srv.Serve(listen); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) Stop(ctx context.Context) {
+	s.srv.GracefulStop()
 }
 
 func getErrorCode(err error) codes.Code {
